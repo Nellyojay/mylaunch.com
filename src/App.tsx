abdb4a5b91da90828f024c3576985f638296c131
Nellyojay <nellyjohnson696@gmail.com>
2026-03-16 02:15:00 +0300

    Mylaunch setup done

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+      <p className='font-bold'>okudi</p>
+
+      <h2 className="text-xl font-bold text-blue-500">My Launch</h2>
+      <div className="bg-red-500">
+
+        <p className="font-bold">damn</p>
+      </div>
+    </>
+  )
+}
+
+export default App
