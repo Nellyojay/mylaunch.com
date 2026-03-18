e54bf48d7df117ee6289aa0a44e73bd2ff65b27b
Nellyojay <nellyjohnson696@gmail.com>
2026-03-19 00:15:43 +0300

    Remove access token

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,13 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {
+    if (window.location.hash.includes("access_token")) {
+      window.history.replaceState({}, document.title, "/")
+    }
+  }, [])
+
   return <RouterProvider router={router} />;
 }
