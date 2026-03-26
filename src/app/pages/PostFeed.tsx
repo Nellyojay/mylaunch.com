1c2a2d66b0aa5b302207570182204cef886df8b6
Nellyojay <nellyjohnson696@gmail.com>
2026-03-26 04:55:34 +0300

    Included the search bar in posts page

--- src/app/pages/PostFeed.tsx
+++ src/app/pages/PostFeed.tsx
@@ -6,9 +6,11 @@ import Loader from "../constants/loader";
 import { useUserData } from "../contexts/userDataContext";
 import { Link } from "react-router-dom";
 import { getImageUrl } from "../constants/imageHandler";
-import { Bookmark } from "lucide-react";
+import { Bookmark, User } from "lucide-react";
+import { useAuth } from "../contexts/authContext";
 
 function PostFeed() {
+  const { session } = useAuth();
   const { currentUser, setSelectedProfile } = useUserData();
   const { loadingPosts, posts, handleDeletePost, fetchStartupPosts, startupData } = useStartup();
   const userStartups = startupData?.filter(s => s.user_id === currentUser?.id)
@@ -23,49 +25,56 @@ function PostFeed() {
 
   return (
     <div className="bg-gray-100 min-h-screen pt-12">
-      <Navbar />
+      <Navbar showSearch={true} />
 
       <div className="max-w-6xl mx-auto px-4 py-6 grid grid-cols-12 gap-4">
 
         <div className="sticky top-18 h-fit hidden md:block col-span-12 md:col-span-3 space-y-2">
 
           <div className="bg-white rounded-md shadow-sm p-4">
-            <Link to={'/profile'} onClick={() => setSelectedProfile(currentUser?.id)}>
-              <img
-                src={getImageUrl(currentUser?.profile_image) || undefined}
-                alt="Profile-image"
-                className="w-12 h-12 rounded-full object-cover"
-              />
-              <p className="font-semibold text-lg">{currentUser?.full_name}</p>
-            </Link>
-
+            {session ? (
+              <Link to={'/profile'} onClick={() => setSelectedProfile(currentUser?.id)}>
+                <img
+                  src={getImageUrl(currentUser?.profile_image) || undefined}
+                  alt="Profile-image"
+                  className="w-12 h-12 rounded-full object-cover"
+                />
+                <p className="font-semibold text-lg">{currentUser?.full_name}</p>
+              </Link>
+            ) : (
+              <Link to={'/login'} className="gap-2">
+                <User className="w-10 h-10 rounded-full object-cover" />
+                <p className="font-semibold text-gray-500">Sign in to view profile</p>
+              </Link>
+            )}
 
           </div>
 
-          <div className="bg-white rounded-md shadow-sm p-4 space-y-2">
-            <p className="font-semibold">Businesses ({userStartups?.length})</p>
+          {session && (
+            <div className="bg-white rounded-md shadow-sm p-4 space-y-2">
+              <p className="font-semibold">Businesses ({userStartups?.length})</p>
 
-            <div>
-              {(userStartups?.length ?? 0) > 0 ? (
-                <>
-                  {userStartups?.map(s => (
-                    <Link
-                      to={`/startup/${s.id}`}
-                      className="line-clamp-1 text-xs text-gray-500 hover:underline"
-                    >
-                      {s.name}
-                    </Link>
-                  ))}
-                </>
-              ) : null}
+              <div>
+                {(userStartups?.length ?? 0) > 0 ? (
+                  <>
+                    {userStartups?.map(s => (
+                      <Link
+                        to={`/startup/${s.id}`}
+                        className="line-clamp-1 text-xs text-gray-500 hover:underline"
+                      >
+                        {s.name}
+                      </Link>
+                    ))}
+                  </>
+                ) : null}
+              </div>
+
+              <div className="flex gap-2">
+                <Bookmark className="fill-black" size={18} />
+                <p className="font-semibold text-xs">Saved</p>
+              </div>
             </div>
-
-            <div className="flex gap-2">
-              <Bookmark className="fill-black" size={18} />
-              <p className="font-semibold text-xs">Saved</p>
-            </div>
-          </div>
-
+          )}
 
         </div>
 
